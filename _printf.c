3c3ea1ff16e400ab58912db6f89f0dc1c56fc1d5
Hamdi-larbi <larbi.hamdi@gmail.com>
2023-02-10 17:50:12 -0800

    update

--- _printf.c
+++ _printf.c
@@ -14,9 +14,10 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int num, k, len, l, count = 0;
-	int i, j, length = 0;
-	char c, *p, *ptr, *dig;
+	int count = 0;
+	int i, j;
+	int length;
+	char c, *p, *ptr;
 
 	va_start(args, format);
 	if (format == NULL)
@@ -26,60 +27,39 @@ int _printf(const char *format, ...)
 		if (format[i] == '%')
 		{
 			i++;
-			switch (format[i])
+			if (format[i] == 'c')
 			{
-				case 'c':
-				{
-					c = va_arg(args, int);
-					count += write(1, &c, 1);
-				}
-				case 's':
-				{
-					ptr = va_arg(args, char *);
-					if (ptr == NULL)
-						count += write(1, "(null)", 6);
-					else
-					{
-						while (*ptr != '\0')
-						{
-							length++;
-							ptr++;
-						}
-						p = malloc((length + 1) * sizeof(char));
-						for (j = 0; j < (length + 1); j++)
-							p[length - j] = *(ptr - j);
-						count += write(1, p, length);
-						free(p);
-						p = NULL;
-						ptr = NULL;
-					}
-				}
-				case '%':
-					count += write(1, &format[i], 1);
-				case 'd':
-				case 'i':
-				{
-					num = va_arg(args, int);
-					k = num;
-					len = 0;
-					while (k >= 1)
-					{
-						len++;
-						k /= 10;
-					}
-					dig = malloc(len + 1);
-					dig[len] = '\0';
-					for (l = 1; l < len + 1; l++)
-					{
-						dig[len - l] = (num % 10) + '0';
-						num /= 10;
-					}
-					count += write(1, dig, len);
-					free(dig);
-					dig = NULL;
-				}
+				c = va_arg(args, int);
+				count += write(1, &c, 1);
 			}
-		else
+			if (format[i] == 's')
+			{
+				ptr = va_arg(args, char*);
+				length = 0;
+				while (*ptr != '\0')
+				{
+					length++;
+					ptr++;
+				}
+				p = malloc((length + 1) * sizeof(char));
+				for (j = 0; j < (length + 1); j++)
+				{
+					p[length - j] = *(ptr - j);
+				}
+				count += write(1, p, length);
+				free(p);
+				p = NULL;
+		/**		size_t length;
+				for (length = 0; ptr[length]; length++) 
+				{
+					;
+		  		}
+			 	count += write(1, ptr, length);*/
+			}
+		}	
+			if (format[i] == '%')
+				count += write(1, &format[i], 1);
+		else	
 			count += write(1, &format[i], 1);
 	}
 	va_end(args);
